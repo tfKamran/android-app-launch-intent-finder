71b9d2553d0880d31d2db7e3876cb888e8687fcf
Kamran Ahmed Ansari <team.radiant@gmail.com>
2018-05-11 18:27:06 +0530

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
 # android-app-launch-intent-finder
+
+[![npm version](https://badge.fury.io/js/android-app-launch-intent-finder.svg)](https://badge.fury.io/js/android-app-launch-intent-finder)
+[![npm downloads](https://img.shields.io/npm/dt/android-app-launch-intent-finder.svg)](https://www.npmjs.com/package/android-app-launch-intent-finder)
+
+[![NPM](https://nodei.co/npm/android-app-launch-intent-finder.png?downloads=true&downloadRank=true&stars=true)](https://nodei.co/npm/android-app-launch-intent-finder/)
+
 Finds launch intents in an Android application APK.
+
+## How to install?
+
+You need to have Node.js installed on your system before you can use this package. Get it here: [Node.js](https://nodejs.org/)
+
+Once you have Node.js and NPM setup, the installation is as simple as running a command.
+
+### Linux/Mac
+
+    sudo npm install -g android-app-launch-intent-finder
+
+### Windows
+
+Within a command prompt window with administrative privileges:
+
+    npm install -g android-app-launch-intent-finder
+
+## How to use?
+
+Type the following command to analyse the launch intents of an app:
+
+    launch-intent-finder my-sample-app.apk
---
+++ index.js
@@ -0,0 +1,113 @@
+#! /usr/bin/env node
+
+const shell = require('shelljs');
+const fs = require('fs');
+const xmlToJSON = require('xml-js');
+const chalk = require('chalk');
+
+function main() {
+    // Check for input arguments
+    if (process.argv.length != 3) {
+        console.log(
+            "You need to pass in an argument with a path of the APK to be analyzed\n"
+            + "\n"
+            + "Example:\n"
+            + "\n"
+            + "\tlaunch-intent-finder workspace/test.apk\n"
+            );
+
+        return;
+    }
+
+    // Cleanup if required
+    deleteIfExists('workspace/app.apk');
+    deleteIfExists('app');
+    deleteIfExists('workspace/app');
+
+    // Decompile the APK
+    shell.exec('cp ' + process.argv[2] + ' workspace/app.apk');
+    shell.exec('java -jar tools/apktool_2.2.1.jar -f d workspace/app.apk');
+    shell.exec('mv app workspace/');
+
+    // Look for launch intents
+    const manifest = fs.readFileSync('workspace/app/AndroidManifest.xml', 'utf-8');
+
+    if (manifest.indexOf('android.intent.category.LAUNCHER') === -1) {
+        console.log('No launcher activity found in the provided APK');
+        return;
+    }
+
+    const manifestJSON = JSON.parse(xmlToJSON.xml2json(manifest));
+    const application = manifestJSON.elements[0].elements.filter(element => element.name == 'application')[0];
+    const activities = application.elements.filter(element => element.name == 'activity' && element.elements
+        && (element.attributes['android:exported'] || isLauncherActivity(element)));
+
+    console.log(getDivider());
+
+    activities.forEach(activity => {
+        var activityHeader = '\nActivity: ' + activity.attributes['android:name'];
+        const isFoundToBeLauncherActivity = isLauncherActivity(activity);
+
+        if (isFoundToBeLauncherActivity) {
+            activityHeader = chalk.hex('#00FF00').bold(activityHeader);
+        } else {
+            activityHeader = chalk.hex('#A0A0FF')(activityHeader);
+        }
+
+        console.log(activityHeader);
+
+        activity.elements.forEach(intentFilters => {
+            console.log('');
+
+            if (intentFilters.elements) {
+                intentFilters.elements.forEach(intentFilter => {
+                    var filter = intentFilter.name + ': ';
+
+                    Object.keys(intentFilter.attributes).forEach(key => {
+                        filter += key.split(':')[1] + '="' + intentFilter.attributes[key] + '", ';
+                    });
+
+                    if (isFoundToBeLauncherActivity) {
+                        filter = chalk.bold(filter);
+                    }
+
+                    console.log(filter);
+                });
+            }
+        });
+
+        console.log('\n' + getDivider());
+    });
+}
+
+function deleteIfExists(file) {
+    if (fs.existsSync(file)) {
+        shell.exec('rm -rf ' + file);
+    }
+}
+
+function isLauncherActivity(activity) {
+    var isLauncherActivity = false;
+
+    activity.elements.forEach(intentFilters => {
+        if (intentFilters.elements) {
+            intentFilters.elements.forEach(intentFilter => {
+                var filter = intentFilter.name + ': ';
+
+                Object.keys(intentFilter.attributes).forEach(key => {
+                    if (intentFilter.attributes[key] === 'android.intent.category.LAUNCHER') {
+                        isLauncherActivity = true;
+                    }
+                });
+            });
+        }
+    });
+
+    return isLauncherActivity;
+}
+
+function getDivider() {
+    return chalk.grey('--------------------------------------------------------------------------------------------');
+}
+
+main();
